7478df4644f9f88fa77bf7bedd2e318c245c5341
Jim Newton <jnewton@lrde.epita.fr>
2021-07-26 15:14:25 +0200

    remove unused function

--- .clj-kondo/def_hook.clj
+++ .clj-kondo/def_hook.clj
@@ -7,18 +7,6 @@
   (:require [clj-kondo.hooks-api :as api]
 ))
 
-(defn transform-def [{:keys [:ns :node]}]
-  (let [[name-node & arg-nodes] (rest (:children node))
-        name-sym (api/sexpr name-node)]
-    (when-not (simple-symbol? name-sym)
-      (let [new-node (with-meta
-                       (api/list-node
-                        (list*
-                         (api/token-node 'def)
-                         (api/token-node (symbol (name name-sym)))
-                         arg-nodes))
-                       (meta node))]
-        {:node new-node}))))
 
 (defmacro print-vals ""
   [& args]
