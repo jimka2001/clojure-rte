a796894aff37fa15027278b051b365b797b626a9
Jim Newton <jnewton@lrde.epita.fr>
2024-05-24 17:58:36 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://opensource.org/licenses/MIT"}
   :plugins [[lein-cloverage "1.1.2"]
             [lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+            [cider/cider-nrepl "0.30.0"]
             ]
   :jvm-opts ["-Xmx1g" "-XX:+HeapDumpOnOutOfMemoryError" "-Djdk.attach.allowAttachSelf"
              ;; increase stack size x6, for preventing SO errors:
@@ -13,10 +14,19 @@
              "-Xss6144k"
              ;; Prevents trivial StackOverflow errors:
              "-XX:MaxJavaStackTraceDepth=1000000"             
+
+             ;; the --add-opens= is for supressing the following warnings
+             ;; WARNING: An illegal reflective access operation has occurred
+             ;; WARNING: Illegal reflective access by clojure.lang.InjectedInvoker/0x0000000800232040 (file:/Users/jimka/.m2/repository/org/clojure/clojure/1.10.3/clojure-1.10.3.jar) to method com.sun.xml.internal.stream.writers.XMLStreamWriterImpl.writeCharacters(java.lang.String)
+             ;; WARNING: Please consider reporting this to the maintainers of clojure.lang.InjectedInvoker/0x0000000800232040
+             ;; WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+             ;; WARNING: All illegal access operations will be denied in a future release
+
+             "--add-opens=java.xml/com.sun.xml.internal.stream.writers=ALL-UNNAMED"
              ]
 
   
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.13"]
                  [lein-cloverage "1.1.2"]
                  [org.clojure/data.json "1.0.0"]
                  [com.clojure-goes-fast/clj-async-profiler "0.5.1"]
@@ -25,4 +35,5 @@
                  [org.clojure/math.combinatorics "0.1.6"]]
   :main ^:skip-aot clojure-rte.rte-core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:test {:plugins [[lein-test-report-junit-xml "0.2.0"]]}
+             :uberjar {:aot :all}})
