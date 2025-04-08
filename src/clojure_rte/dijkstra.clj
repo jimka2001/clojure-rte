bd417022c4f2d3c4ee3258513293b7ddffc5e384
Jim Newton <jim.newton@epita.fr>
2025-04-08 08:44:23 +0200

    added code suggested by https://clojurians.slack.com/team/U2FRKM4TW

--- src/clojure_rte/dijkstra.clj
+++ src/clojure_rte/dijkstra.clj
@@ -45,3 +45,35 @@
         :otherwise
         (let [dist (-> (f v) (remove-keys r) (map-vals (partial + d)))]
           (recur (merge-with min (pop q) dist) (assoc r v d)))))))
+
+#_((require '[clojure.data.priority-map :refer [priority-map]])
+
+;; code suggested by Eugene Pakhomov, https://clojurians.slack.com/team/U2FRKM4TW
+;; https://clojurians.slack.com/archives/C03S1KBA2/p1744060041343069?thread_ts=1744055580.190099&cid=C03S1KBA2
+
+
+(defn remove-keys [m pred]
+  (select-keys m (remove pred (keys m))))
+
+(defn dijkstra
+  "Computes single-source shortest path distances in a directed graph.
+
+  Given a node n, (f n) should return a map with the successors of n
+  as keys and their (non-negative) distance from n as vals.
+
+  Returns a vector with a map from nodes to their distance from start and a map of prev nodes."
+  [start f]
+  (loop [q (priority-map start 0)
+         r {}
+         p {}]
+    (if-let [[v d] (peek q)]
+      (let [dist (-> (f v) (remove-keys r) (update-vals #(+ d %)))
+            [q p] (reduce-kv (fn [[q p] n d]
+                               (let [curr-d (q n)]
+                                 (if (or (nil? curr-d) (< d curr-d))
+                                   [(assoc q n d) (assoc p n v)]
+                                   [q p])))
+                             [(pop q) p]
+                             dist)]
+        (recur q (assoc r v d) p))
+      [r p]))))
