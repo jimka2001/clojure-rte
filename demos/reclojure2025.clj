64029b359c6f4e04b07d8ccc51d26f9c8ffd8e64
Jim Newton <jimka2001@users.noreply.github.com>
2025-04-25 18:07:35 +0200

    extending demo explanation

--- demos/reclojure2025.clj
+++ demos/reclojure2025.clj
@@ -6,6 +6,10 @@
             [dot])
 )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Multi-arity functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Definition of multiple-arity function
 (defn f
   ([a]   12)
@@ -32,12 +36,43 @@
 (f true 2)
 (f 1 2)
 
+(defn f
+  ([a]   12)
+  ([^Ratio a b]   13)
+  ([a b [c d]]   14))
+
+
+(defn f
+  ([a]   12)
+  ([a b]   13)
+  ([a b [^int? c ^Ratio d]]   14))
+
+(f 0 1 [3 52/17])       ;; --> 14
+(f 0 1 ["hello" false]) ;; --> 14
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Functions defined by dsdefn
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(dsdefn f 
+  ([[a b] c d] 12)
+  ([a [b c] d] 13)
+  ([a b [c d]] 14))
+
+(f [0 1] 2 3)  ;; --> 12
+(f 0 [1 2] 3)  ;; --> 13
+(f 0 1 [2 3])  ;; --> 14
+
+
 
 (dsdefn f 
   ([[a b] c d] 12)
   ([a [b c] d] 13)
   ([a b [c d]] 14)
-  ([a b [c d]] 15))
+  ([a b [c d]] (println "fourth clause") 15))
 
 
 (dsdefn f 
@@ -70,64 +105,45 @@
   ([[a b] c d] 12)
   ([a [b c] d] 13)
   ([a b [^Ratio c d]] 14)
-  ([a b [^integer? c d]] 15)
+  ([a b [^int? c d]] 15)
   ([a b [^Number c d]] 16)
   ([a b [^Double c d]] 17))
-
-(def f
-  (let [dfa__51157__auto__ (rte-case/clauses-to-dfa
-                            '([0
-                               (:cat
-                                (rte
-                                 (:cat
-                                  (and :sigma :sigma)
-                                  (and :sigma :sigma)))
-                                (and :sigma :sigma)
-                                (and :sigma :sigma))]
-                              [1
-                               (:cat
-                                (and :sigma :sigma)
-                                (rte
-                                 (:cat
-                                  (and :sigma :sigma)
-                                  (and :sigma :sigma)))
-                                (and :sigma :sigma))]
-                              [2
-                               (:cat
-                                (and :sigma :sigma)
-                                (and :sigma :sigma)
-                                (rte
-                                 (:cat
-                                  (and Ratio :sigma)
-                                  (and :sigma :sigma))))]))]
-    (fn [& seq__51158__auto__]
-      (let [fns__51159__auto__ [(fn conv-1 [[^integer? a ^integer? b] c d] 12)
-                                (fn conv-1 [a [b c] d] 13)
-                                (fn conv-1 [a b [^Ratio c ^integer? d]] 14)]
-            ind__51160__auto__ (rte/match
-                                dfa__51157__auto__
-                                seq__51158__auto__
-                                :promise-disjoint
-                                true)]
-        (if ind__51160__auto__
-          (apply
-           (fns__51159__auto__ ind__51160__auto__)
-           seq__51158__auto__)
-          (throw
-           (ex-info
-            "No pattern matching given list"
-            {:sequence seq__51158__auto__})))))))
-
-(fn [a b [c d]] nil)
 
 (dsdefn f 
   ([[a b] c d] 12)
   ([a [b c] d] 13)
   ([a b [^Ratio c d]] 14)
-  (^{c (satisfies integer?)} [a b [c d]] 15)
+  ([a b [^(satisfies int?) c d]] 15)
+  ([a b [^Number c d]] 16)
+  ([a b [^Double c d]] 17))
+
+(dsdefn f 
+  ([[a b] c d] 12)
+  ([a [b c] d] 13)
+  ([a b [^Ratio c d]] 14)
+  (^{c (satisfies int?)} [a b [c d]] 15)
   ([a b [^Double c d]] 16))
 
 (f 1 2 [10 20])
+(f 1 2 [10.0 20])
+(f 1 2 [10/3 20])
+(f 1 2 [false 20])
+
+;; In selecting which clause to evaluate, a computation is done using a
+;; Dfa.  The Dfa is build by the macro expansion (when the macro expansion
+;; is evaluated).  When the function f is called the Dfa is traversed
+;; once to determine which piece of code to evalu
+
+(dsdefn f 
+  ([^Number a b] 14)
+  (^{a Boolean
+     c (satisfies int?)} [a b c d] 15)
+  ( [a b ^Double c d] 16))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rte-case
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (rte-case [nil nil nil 1 nil nil nil]
@@ -152,6 +168,10 @@
   2
 )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  rte/match
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; we want to match a sequence of numbers.
 
 (def demo-seq [1 2 4/3 4 5 6.5 3 2])
@@ -173,24 +193,21 @@
            demo-seq)
 
 
+
 ;; what about a seqence of numbers which contains a Double AND a Ratio
-;; in either order
-(rte/match '(:or (:cat (:* Number) Double (:* Number) Ratio (:* Number))
-                 (:cat (:* Number) Ratio (:* Number) Double (:* Number)))
-           demo-seq)
-
-(rte/match '(:and (:cat (:* Number) Double (:* Number))
-                  (:cat (:* Number) Ratio (:* Number)))
-           demo-seq)
-
+;; in either order.  
 
 ;; First way to express seq of Number which contains both a Ratio
-;; and a Double in either order.
+;; and a Double
 (def rte-1 '(:and (:cat (:* Number) Double (:* Number))
                   (:cat (:* Number) Ratio (:* Number))))
 
-(dot/dfa-to-dot rte-1 :title "first dfa"
+(rte/match rte-1 demo-seq)
+
+
+(dot/dfa-to-dot rte-1 :title "Dfa rte-1"
                 :view true
+                :dot-file-cb println
                 :state-legend false)
 
 
@@ -200,14 +217,16 @@
 (def rte-2 '(:or (:cat (:* Number) Double (:* Number) Ratio (:* Number))
                  (:cat (:* Number) Ratio (:* Number) Double (:* Number))))
 
-(dot/dfa-to-dot rte-2 :title "second dfa" 
+(dot/dfa-to-dot rte-2 :title "Dfa rte-2" 
                 :view true
+                :dot-file-cb println
                 :state-legend false)
 
 ;; are the rtes the same?  do have match the same language?
 
 (dot/dfa-to-dot (rte/Xor rte-1 rte-2)
                 :title "xor 1-2 dfa"
+                :dot-file-cb println
                 :state-legend false
                 :view true)                         
 
@@ -215,8 +234,7 @@
 ;; Third way of expression the same thing (with suble error)
 
 (def rte-3 '(:and (:* Number)
-                  (:cat (:* :sigma) Ratio (:* :sigma))
-                  (:cat (:+ :sigma) Double (:* :sigma))))
+                  (:cat (:* :sigma) (or Ratio Double) (:* :sigma))))
 
 (dot/dfa-to-dot rte-3 :title "third dfa" 
                 :view true
@@ -234,11 +252,12 @@
                   (:cat (:* :sigma) Ratio (:* :sigma))
                   (:cat (:+ :sigma) (satisfies odd?) (:* :sigma))))
 
-(xym/find-trace-map (rte/rte-to-dfa rte-4))
+(println (xym/find-trace-map (rte/rte-to-dfa (rte/And-not rte-1 rte-4))))
 
-(dot/dfa-to-dot rte-4
-                :title "odd and ratio"
+(dot/dfa-to-dot (rte/And-not rte-1 rte-4)
+                :title "rte and-not"
                 :state-legend false
+                :dot-file-cb println
                 :view true)
 
 ;; a sequence of numbers which contains an integer and a Ratio
@@ -250,7 +269,7 @@
 (xym/find-trace-map (rte/rte-to-dfa rte-5))
 
 (dot/dfa-to-dot rte-5
-                :title "intr and ratio"
+                :title "int and ratio"
                 :state-legend false
                 :view true)
 
