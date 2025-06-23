aa3659f7cdf259b287be812304aabcc08a6777dd
Jim Newton <jimka2001@users.noreply.github.com>
2025-06-23 14:34:29 +0200

    added some tests

---
+++ test/statistics_test.clj
@@ -0,0 +1,47 @@
+;; Copyright (c) 2025 EPITA Research and Development Laboratory
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without restriction,
+;; including without limitation the rights to use, copy, modify, merge,
+;; publish, distribute, sublicense, and/or sell copies of the Software,
+;; and to permit persons to whom the Software is furnished to do so,
+;; subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(ns statistics-test
+  (:require [clojure.pprint :refer [cl-format]]
+            [statistics :as sut]
+            [util :refer [human-readable-current-time]]
+            [clojure.test :refer [deftest is] :exclude [testing]]
+))
+
+(def test-verbose false)
+
+(defmacro testing
+  [string & body]
+  `(do
+     (when test-verbose
+       (println [:testing 'rte-test ~string :starting (human-readable-current-time)]))
+     (clojure.test/testing ~string ~@body)
+     (when test-verbose
+       (println [:finished ' rte-test ~string (human-readable-current-time)]))))
+
+(deftest t-read-csv-resource
+  (testing "reading csv resource"
+    (is (sut/read-resource-csv sut/subset-csv))
+    (is (sut/read-resource-csv sut/inhabited-csv))))
+
+(deftest t-update-resource-csv
+  (testing "update csv resource"
+    (sut/update-resource-csv 2)))
