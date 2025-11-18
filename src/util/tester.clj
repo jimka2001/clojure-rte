bfa338803e8451b9158ef8d470f9e5a427b11e67
Jim Newton <jim.newton@epita.fr>
2025-11-18 13:32:52 +0100

    moved file

--- src/util/tester.clj
+++ src/util/tester.clj
@@ -19,7 +19,7 @@
 ;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 ;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-(ns tester
+(ns util.tester
   (:require [clojure.pprint :refer [cl-format]]
             [util.util :refer [map-eagerly]])
   )
