f5d5dbabfe3ec7d7fd8b624cb2be31b67ff279a3
Jim Newton <jim.newton@epita.fr>
2025-12-01 10:06:16 +0100

    tests

--- test/conj_2025_test.clj
+++ test/conj_2025_test.clj
@@ -1,8 +1,21 @@
 (ns conj-2025-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.test :refer [deftest is]]
             [demos.conj-2025.cli :as cli]
+            [rte-construct            :as rte]
+            [util.util :refer [human-readable-current-time]]
             [demos.conj-2025.gen            :as gen]
             [demos.conj-2025.plot :as sut]))
+
+(def test-verbose true)
+
+(defmacro testing
+  [string & body]
+  `(do (when test-verbose
+         (println [:testing ~string :starting (human-readable-current-time)]))
+       (rte/with-compile-env []
+         (clojure.test/testing ~string ~@body))
+       (when test-verbose
+         (println [:finished  (human-readable-current-time)]))))
 
 (deftest histogram
   (testing "histogram"
@@ -23,7 +36,7 @@
 
 (deftest sample-view
   (testing "sample-view"
-    (gen/sample-view 28)))
+    (gen/sample-view 10)))
 
 (deftest gen-by-size
   (testing "gen-by-size"
